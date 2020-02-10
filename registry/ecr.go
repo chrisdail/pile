3c7df633143f2afcda05eb28710325e9af32fdba
Chris Dail <Chris.Dail@akiri.com>
2020-02-10 11:10:35 -0400

    Implement querying for images via ECR

--- registry/ecr.go
+++ registry/ecr.go
@@ -1,6 +1,13 @@
 package registry
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ecr"
+)
 
 const DefaultECRRegion = "us-east-1"
 
@@ -9,14 +16,40 @@ type AmazonECR struct {
 	Region    string
 }
 
-func (registry *AmazonECR) Prefix() string {
-	region := registry.Region
-	if region == "" {
-		region = DefaultECRRegion
+func (registry *AmazonECR) region() string {
+	if registry.Region != "" {
+		return registry.Region
 	}
-	return fmt.Sprintf("%s.dkr.ecr.%s.amazonaws.com/", registry.AccountID, region)
+	return DefaultECRRegion
+}
+
+func (registry *AmazonECR) Prefix() string {
+	return fmt.Sprintf("%s.dkr.ecr.%s.amazonaws.com/", registry.AccountID, registry.region())
 }
 
 func (registry *AmazonECR) Contains(repository string, tag string) bool {
+	sess := session.Must(session.NewSession(aws.NewConfig().WithRegion(registry.region())))
+	svc := ecr.New(sess)
+
+	output, err := svc.DescribeImages(&ecr.DescribeImagesInput{
+		RegistryId:     &registry.AccountID,
+		RepositoryName: &repository,
+		ImageIds:       []*ecr.ImageIdentifier{&ecr.ImageIdentifier{ImageTag: &tag}},
+	})
+	if err != nil {
+		log.Println(fmt.Errorf("Error searching for image %s:%s in ECR: %v", repository, tag, err))
+		return false
+	}
+
+	for _, detail := range output.ImageDetails {
+		log.Printf("Image in registry %s:%s found, pushedAt %s, digest: %s",
+			repository,
+			tag,
+			detail.ImagePushedAt,
+			detail.ImageDigest,
+		)
+		return true
+	}
+
 	return false
 }
